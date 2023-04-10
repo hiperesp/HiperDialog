398bfb0282fc3219540b3a3f3bace83a3d3dda01
Gabriel Lopes <gabriel@gabstep.com.br>
2023-04-09 21:48:17 -0300

    Change OK and Cancel button for extra functions

--- src/js/extra.js
+++ src/js/extra.js
@@ -1,14 +1,14 @@
 "use strict";
-async function alertDialog(text, title) {
+async function alertDialog(text, title, okButtonText = "OK") {
     const dialog = new Dialog(title, text);
-    dialog.addElement(/*0*/new DialogButton("OK", "positive", undefined, true));
+    dialog.addElement(/*0*/new DialogButton(okButtonText, "positive", undefined, true));
     await dialog.prompt();
 }
-async function confirmDialog(text, title) {
+async function confirmDialog(text, title, okButtonText = "OK", cancelButtonText = "Cancel") {
     const dialog = new Dialog(title, text);
     dialog.addElement(
-        /*0*/new DialogButton("OK", "positive", undefined, true)
-       ,/*1*/new DialogButton("Cancelar", "negative")
+        /*0*/new DialogButton(okButtonText, "positive", undefined, true)
+       ,/*1*/new DialogButton(cancelButtonText, "negative")
     );
     const answer = await dialog.prompt();
     if(answer==0) {
@@ -16,13 +16,13 @@ async function confirmDialog(text, title) {
     }
     return false;
 }
-async function promptDialog(text, defaultValue, title, placeholder) {
+async function promptDialog(text, defaultValue, title, placeholder, okButtonText = "OK", cancelButtonText = "Cancel") {
     const input = new DialogInput(placeholder, defaultValue);
     const dialog = new Dialog(title, text);
     dialog.addElement(
         /*0*/input
-       ,/*1*/new DialogButton("OK", "positive", undefined, true)
-       ,/*2*/new DialogButton("Cancelar", "negative")
+       ,/*1*/new DialogButton(okButtonText, "positive", undefined, true)
+       ,/*2*/new DialogButton(cancelButtonText, "negative")
     );
     const answer = await dialog.prompt();
     if(answer==1) {
